b96c6f2f3d3fff1735cdcd48744058ec0ccf18e0
a-adegite <adegiteadejuwon@gmail.com>
2025-10-11 14:08:57 +0100

    updated data download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -20,6 +20,9 @@ set -euo pipefail
 # Directory where downloaded FASTQ files will be stored
 data_dir="../data/raw_reads/"
 
+# create directory if it doesn't exist
+mkdir -p "${data_dir}"
+
 # Chronic PJI samples (C)
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/076/SRR20959676/SRR20959676_1.fastq.gz -o "${data_dir}SRR20959676_C_1.fastq.gz"
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/076/SRR20959676/SRR20959676_2.fastq.gz -o "${data_dir}SRR20959676_C_2.fastq.gz"
@@ -27,8 +30,6 @@ curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/077/SRR20959677/SRR209596
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/077/SRR20959677/SRR20959677_2.fastq.gz -o "${data_dir}SRR20959677_C_2.fastq.gz"
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/078/SRR20959678/SRR20959678_1.fastq.gz -o "${data_dir}SRR20959678_C_1.fastq.gz"
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/078/SRR20959678/SRR20959678_2.fastq.gz -o "${data_dir}SRR20959678_C_2.fastq.gz"
-curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/079/SRR20959679/SRR20959679_1.fastq.gz -o "${data_dir}SRR20959679_C_1.fastq.gz"
-curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/079/SRR20959679/SRR20959679_2.fastq.gz -o "${data_dir}SRR20959679_C_2.fastq.gz"
 
 # Acute PJI samples (A)
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/080/SRR20959680/SRR20959680_1.fastq.gz -o "${data_dir}SRR20959680_A_1.fastq.gz"
@@ -37,7 +38,5 @@ curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/081/SRR20959681/SRR209596
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/081/SRR20959681/SRR20959681_2.fastq.gz -o "${data_dir}SRR20959681_A_2.fastq.gz"
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/082/SRR20959682/SRR20959682_1.fastq.gz -o "${data_dir}SRR20959682_A_1.fastq.gz"
 curl -L -C - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR209/082/SRR20959682/SRR20959682_2.fastq.gz -o "${data_dir}SRR20959682_A_2.fastq.gz"
-
-curl -L -C - ftp:// -o SRR20959683_GSM6435901_A-PJI01_Staphylococcus_aureus_RNA-Seq
 
 echo "Download complete. All acute (A) and chronic (C) paired-end reads stored in ${data_dir}"
