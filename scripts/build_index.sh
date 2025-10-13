c42923a5248295374595a3d37c0a8103fa40137b
a-adegite <adegiteadejuwon@gmail.com>
2025-10-13 13:07:44 +0100

    Script to build S.aureus genome index

---
+++ scripts/build_index.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# directory containing reference genome
+ref_genome="../../refdata/s_aureus_ASM1342v1/s_aureus.fna"
+
+# directory containing generated genome index
+genome_index="../../../refdata/s_aureus_ASM1342v1/star_index"
+
+# directory contation annotation file (GTF)
+ref_gtf="../../refdata/s_aureus_ASM1342v1/s_aureus.gtf"
+
+# create directory if it doesn't exist
+mkdir -p "${genome_index}"
+
+# Generate genome index using STAR
+STAR \
+    --runThreadN 9 \
+    --runMode genomeGenerate \
+    --genomeDir "${genome_index}" \
+    --genomeFastaFiles "${ref_genome}" \
+    --sjdbGTFfile "${ref_gtf}" \
+    --sjdbOverhang 149 \
+    --genomeSAindexNbases 9
